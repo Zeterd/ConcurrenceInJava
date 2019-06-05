4f58523503e0bb77f4212e571fe494e2847a8256
guilherme29 <gui.amado29@gmail.com>
2019-06-05 11:09:34 +0100

    completed push and pop, still haven't tested

--- projeto/src/pc/stack/ALinkedStackASR.java
+++ projeto/src/pc/stack/ALinkedStackASR.java
@@ -4,23 +4,23 @@ import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
  * Lock-free stack with optional exponential back-off scheme.
- * 
+ *
  * @param <E> Type of elements in the stack.
  */
 public class ALinkedStackASR<E> implements Stack<E> {
-  
+
 
   private final AtomicStampedReference<Node<E>> ref;
-  
+
   private final Backoff backoff;
-  
+
   /**
    * Constructor with no arguments, disabling back-off by default.
    */
   public ALinkedStackASR() {
     this(false);
   }
-  
+
   /**
    * Constructor with explicit back-off setting.
    * @param enableBackoff Flag indicating if back-off should be used or not.
@@ -41,15 +41,63 @@ public class ALinkedStackASR<E> implements Stack<E> {
       throw new IllegalArgumentException();
     }
     // TODO
+    Node<E> newNode = new Node<>(elem, null);
+    Node<E> oldNode;
+
+    while(true){
+
+      int[] stampHolder = new int[1];
+      oldNode = ref.get(stampHolder);
+
+      int oldSize = ref.getStamp();
+      newNode.next = oldNode;
+
+      if(ref.compareAndSet(oldNode, newNode, oldSize, stampHolder[0])){
+        if (backoff != null) {
+          backoff.diminish();
+        }
+        break;
+      }
+      if (backoff != null) {
+        backoff.delay();
+      }
+
+    }
   }
 
   @Override
   public E pop() {
     E elem = null;
     // TODO ...
+    Node<E> oldNode;
+    Node<E> newNode;
+
+    while(true){
+      int[] stampHolder = new int[1];
+      oldNode = ref.get(stampHolder);
+
+      if(oldNode == null){
+        elem = null;
+        break;
+      }
+
+      newNode = oldNode.next;
+      int newSize = stampHolder[0] - 1;
+      elem = oldNode.data;
+
+      if(ref.compareAndSet(oldNode, newNode, stampHolder[0], newSize)){
+        if (backoff != null) {
+          backoff.diminish();
+        }
+        break;
+      }
+      if (backoff != null) {
+        backoff.delay();
+      }
+    }
     return elem;
   }
-  
+
   //For tests
   @SuppressWarnings("javadoc")
   public static class Test extends StackTest {
@@ -58,4 +106,5 @@ public class ALinkedStackASR<E> implements Stack<E> {
       return new ALinkedStackASR<>();
     }
   }
-} 
+}
+
